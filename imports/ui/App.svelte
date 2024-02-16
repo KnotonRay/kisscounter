7402be0a4fda1c96f083c458f377a5776d56eba4
Knoton <knoton@gmail.com>
2024-02-16 18:18:29 -0500

    Refinements

--- imports/ui/App.svelte
+++ imports/ui/App.svelte
@@ -6,15 +6,14 @@
 
   Log('App.svelte script');
 
-  function sniff()
-  {
+  var startPos = null;
+
+  function sniff() {
     var sniffcheck = new URLSearchParams(document.location.search).get('sniffcheck');
     sniffcheck = sniffcheck === null ? '' : sniffcheck;
     Log('sniffed "' + sniffcheck + '"');
     return sniffcheck;
-  }
-
-
+  };
 
   const increment = () => {
     Log('increment');
@@ -35,7 +34,76 @@
         decrement();
 				break;
 		 }
-} 
+  }
+
+  function getPageDimensions() {
+    const pageWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+    const pageHeight = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+    return [pageWidth, pageHeight];
+  }
+
+  function onMouseDown(evt) {
+    onPointerActionStart(evt);
+  }
+
+  function onMouseUp(evt) {
+    onPointerActionEnd(evt);
+  }
+
+  function onTouchStart(evt) {
+    onPointerActionStart(evt.touches[0]);
+  }
+  
+  function onTouchEnd(evt) {
+    onPointerActionEnd(evt.changedTouches[0]);
+  }
+
+  function onPointerActionStart(posSource) {
+    startPos = [posSource.pageX, posSource.pageY];
+  }
+
+  function onPointerActionEnd(posSource) {
+    if (startPos !== null) {
+      const [startX, startY] = startPos;
+      startPos = null;
+      const endX = posSource.pageX;
+      const endY = posSource.pageY;
+
+      const deltaX = endX - startX;
+      const deltaY = endY - startY;
+
+      // click/tap
+      if (Math.abs(deltaX) < 5 && Math.abs(deltaY) < 5) {
+        const [width, height] = getPageDimensions();
+        const halfWidth = width / 2;
+        const halfHeight = height / 2;
+
+        // near screen center
+        if (Math.abs(endX - halfWidth) < 10 && Math.abs(endY - halfHeight) < 100) {
+          increment();
+        }
+
+      // vertical swipe
+      } else if (Math.abs(deltaY) > 200) {
+
+        // swipe down
+        if (deltaX > 0) {
+          decrement();
+
+        // swipe up
+        } else {
+          increment();
+
+        }
+      }
+    }
+  }
+
+  document.addEventListener('mousedown', onMouseDown);
+  document.addEventListener('mouseup', onMouseUp);
+  document.addEventListener('touchstart', onTouchStart);
+  document.addEventListener('touchend', onTouchEnd);
+
 </script>
 
 
@@ -47,14 +115,22 @@
 <svelte:window on:keyup|preventDefault={onKeyUp} />
 
 <style>
+
+:global(html) {
+  overflow: hidden;
+  width: 100%;
+  height: 100%;
+}
+
 :global(body) {
   display: grid;
-  background-color: lightblue;
-  height: 100vh;
+  background: linear-gradient(to top, #0072ff 0%, #00c6ff 100%);
+  height: 100%;
   width: auto;
   box-sizing: border-box;
   padding: 2%;
   margin: 0;
+  overflow: hidden;
 }
 
 .container
@@ -65,14 +141,12 @@
   justify-items: center;
   width: 100%;
   height: 100%;
+  grid-template-rows: 80% min-content 5%;
 }
 
 .attribution
 {
   font-size: 10px;
   color: blue;
-  position: absolute;
-  left: 0px;
-  bottom: 0px;
 }
 </style>
--- imports/ui/Banner.svelte
+++ imports/ui/Banner.svelte
@@ -1,3 +1,31 @@
-<h1>Diapers kissed</h1>
+
+
+<div class='wrap'>
+    <h1>Diapers kissed</h1>
+    <img src='/ribbon.svg'/>
+</div>
 <style>
+    wrap {
+        display: grid;
+        grid-template: 
+            "a"
+    }
+
+    h1 {
+        grid-area: a;
+        position: relative;
+        z-index: 2;
+        text-align: center;
+        margin: 0;
+        margin-bottom: 4%;
+        font-family: "Comic Sans MS", "Comic Sans", cursive;
+        font-size: clamp(1rem, -0.275rem + 8.333vw, 8.5rem);
+        color: lightgoldenrodyellow;
+    }
+
+    img {
+        grid-area: a;
+        max-width: 80vw;
+        margin-top: -100%;
+    }
 </style>
