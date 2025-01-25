ee9a57dacd9e40315b37591dd01664b2c5a9d7a0
KnotonRay <159492213+KnotonRay@users.noreply.github.com>
2025-01-25 16:37:49 -0500

    Update History.svelte

--- imports/ui/History.svelte
+++ imports/ui/History.svelte
@@ -1,13 +1,16 @@
 <p>If you’re not familiar with my efforts to kiss diapers, you can ask pretty much any attendee 
 of the last four CAPCons or BabyFurCon and they will likely recall “the diaper kisser guy” and my "Please may I kiss your diaper?" sign as I seem to have made a positive and memorable impression on many people. </p>
 
-<p>Over the course of the five events I’ve done this at, I’ve kissed over 1,300 diapers from attendees seeing my sign and taking me up on my offer. </p>
+<p>Over the course of the five events I’ve done this at, I’ve kissed over <b>1,900</b> diapers from attendees seeing my sign and taking me up on my offer. </p>
 <ul>
     <li>CAP ‘20: 73</li>
     <li>SummerCAP: 225</li>
     <li>CAP Carnival: 518</li>
-    <li>BFC: 206</li>
+    <li>BFC '23: 206</li>
     <li>CAP '24: 296</li>
+    <li>BFC '24: 326</li>
+    <li>SideQuest: 217</li>
+    <li>Misc events: 40</li>
 </ul>
 <p>
 Each time, I’ve refined the presentation, from commissioning a graphic artist to make a nicer sign, to having a visible count of how many diapers I’ve kissed visible next to me, to putting a small kiss mark sticker on the badge of each person whose diaper I kissed.</p>
